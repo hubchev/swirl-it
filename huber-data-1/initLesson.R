ad7f81fc8d409b213235b0dc1f295ea0a05ce298
hubchev <DrStephanHuber@yahoo.com>
2022-04-12 10:45:39 +0200

    solved a bug in huber-data-1 about openintro and datasets::cars

--- huber-data-1/initLesson.R
+++ huber-data-1/initLesson.R
@@ -1,2 +1,2 @@
-  assign("cars", datasets::cars, envir=globalenv())
-  assign("mpg.midsize", cars[cars$type=="midsize","mpgCity"], envir=globalenv())
+  assign("cars93", openintro::cars93, envir=globalenv())
+  assign("mpg.midsize", cars93[cars93$type=="midsize","mpg_city"], envir=globalenv())
